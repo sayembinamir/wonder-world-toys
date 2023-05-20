9178c30ea5dcfbe2a4a4bf7d74c13768b94a84cd
sayembinamir <sayembinamir912@gmail.com>
2023-05-21 00:19:20 +0600

    log in part

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,7 +11,7 @@ const Login = () =>
     {
         event.preventDefault();
         const form = event.target;
-        const email = form.email.value;
+        // const email = form.email.value;
         const password = form.password.value;
 
         console.log(email, password)
